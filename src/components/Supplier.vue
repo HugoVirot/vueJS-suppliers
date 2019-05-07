30d8f49912aba23022aa74775990002bb993c3ad
Hugo Virot <hugo.virot@gmail.com>
2019-05-07 14:39:55 +0200

    ex 6.0 réussi

--- src/components/Supplier.vue
+++ src/components/Supplier.vue
@@ -1,5 +1,5 @@
 <template>
-<div id="supplier" class=" container w-50 mt-2 border border-success">
+<div id="supplier" class="container-fluid mt-2 border border-success">
 <h3>Fournisseur : {{ name }}</h3>
 <h3 v-if="status===true" class="text-success">A du stock ? OK</h3>
 <h3 v-else class="text-danger">A du stock ? KO</h3>
@@ -11,6 +11,7 @@
 export default {
   name: 'Supplier',
   props: [
+    'id',
     'name',
     'status',
     'checkedAt'
--- src/components/SuppliersList.vue
+++ src/components/SuppliersList.vue
@@ -1,24 +1,38 @@
 
 <template>
-
-<Supplier :name="name" :status='status' :checkedAt="checkedAt"/>
-
+  <ul>
+    <li class="list-unstyled" v-for="supplier in suppliers" :key="supplier.id">
+      
+      <Supplier :name="supplier.name" :status="supplier.status" :checkedAt="supplier.checkedAt"/>
+    </li>
+  </ul>
 </template>
 
 <script>
-import Supplier from './Supplier.vue'
+import Supplier from "./Supplier.vue";
 
 export default {
-  name: 'SuppliersList',
+  name: "SuppliersList",
   components: {
-    Supplier
+    Supplier,
   },
-  data: function(){
-    return {
-    name: "fournisseur",
-    status: true,
-    checkedAt: new Date().toLocaleString()
+  data() {
+    return{
+      suppliers: [
+        {
+          id: 1,
+          name: "Fournisseur 1",
+          status: true,
+          checkedAt: new Date()
+        },
+        {
+          id: 2,
+          name: "Fournisseur 2",
+          status: false,
+          checkedAt: new Date()
+        }
+      ]
     }
-  },
-}
+  }
+};
 </script>
