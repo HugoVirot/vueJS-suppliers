e972c032160cd6b3840367452190686e9d07ea07
Hugo Virot <hugo.virot@gmail.com>
2019-05-10 13:57:05 +0200

    localisation fonctionnelle (étape 9)

--- src/components/SuppliersMap.vue
+++ src/components/SuppliersMap.vue
@@ -1,42 +1,53 @@
 <template>
-<div class="mb-5">
-<gmap-map
-  :center="{lat:45, lng:2}"
-  :zoom="6"
-  style="width: 80%; height: 800px; border: 5px solid #41B883; margin: auto"
->
-<GmapMarker
-    :key="index"
-    v-for="(supplier, index) in suppliers"
-    :position = "{lat: parseFloat(supplier.latitude),lng: parseFloat(supplier.longitude)}"
-    :clickable="true"
-    :draggable="true"
-    @click="center=supplier.position"
-  />
-</gmap-map>
-</div>
+  <div class="mb-5">
+    <gmap-map
+      :center="{lat:userPosition.latitude, lng:userPosition.longitude}"
+      :zoom="14"
+      style="width: 80%; height: 800px; border: 5px solid #41B883; margin: auto"
+    >
+      <GmapMarker
+        :key="index"
+        v-for="(supplier, index) in suppliers"
+        :position="{lat: parseFloat(supplier.latitude),lng: parseFloat(supplier.longitude)}"
+        :clickable="true"
+        :draggable="true"
+        @click="center=supplier.position"
+      />
+    </gmap-map>
+  </div>
 </template>
 
 <script>
-const axios = require('axios');
+const axios = require("axios");
 
-import { format, render, cancel, register } from 'timeago.js';
+import { format, render, cancel, register } from "timeago.js";
 export default {
-  name: 'SuppliersMap',
-    data() {
-    return{
+  name: "SuppliersMap",
+  data() {
+    return {
       suppliers: [],
       loading: false,
       error: null,
-    }
+      userPosition: { latitude: 0, longitude: 0 }
+    };
   },
-    created () {
+  created() {
     axios
-      .get('https://api-suppliers.herokuapp.com/api/suppliers')
-      .then(response => {this.suppliers = response.data})
-      .catch(error => {console.log(error)
-      this.errored = true})
-      .finally(() => this.loading = false)
+      .get("https://api-suppliers.herokuapp.com/api/suppliers")
+      .then(response => {
+        this.suppliers = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+        this.errored = true;
+      })
+      .finally(() => (this.loading = false));
+  },
+  mounted() {   //méthode 1 : fonction fléchée, méthode 2 : fonction normale, self=this
+    navigator.geolocation.getCurrentPosition((position)=> {
+      this.userPosition = position.coords;
+      console.log(this.userPosition);
+    });
   }
-}
+};
 </script>
