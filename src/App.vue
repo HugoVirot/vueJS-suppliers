a07f86a9853d2c1aaa8e13a4f8ceeb2c6cf25529
Hugo Virot <hugo.virot@gmail.com>
2019-05-10 10:44:35 +0200

    marqueurs des fournisseurs en place

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
     <button id="carte" v-on:click="onMapClick">Voir la carte</button>
     <Suppliers></Suppliers>
     <Map></Map>-->
-    <router-link to="/suppliers"><button class="btn btn-success mr-1 mb-2">fournisseurs</button></router-link>
-    <router-link to="/map"><button class="btn btn-success mb-2">carte</button></router-link>
+    <router-link to="/suppliers"><button style="background-color: #41B883" class="btn btn-success mr-1 mb-2">fournisseurs</button></router-link>
+    <router-link to="/map"><button style="background-color: #41B883" class="btn btn-success mb-2">carte</button></router-link>
     <router-view></router-view>
   </div>
 </template>
--- src/components/SuppliersMap.vue
+++ src/components/SuppliersMap.vue
@@ -1,23 +1,25 @@
 <template>
-
+<div class="mb-5">
 <gmap-map
   :center="{lat:45, lng:2}"
   :zoom="6"
-  style="width: 100%; height: 800px"
+  style="width: 80%; height: 800px; border: 5px solid #41B883; margin: auto"
 >
 <GmapMarker
     :key="index"
     v-for="(supplier, index) in suppliers"
-    :position="supplier.position"
+    :position = "{lat: parseFloat(supplier.latitude),lng: parseFloat(supplier.longitude)}"
     :clickable="true"
     :draggable="true"
     @click="center=supplier.position"
   />
 </gmap-map>
-
+</div>
 </template>
 
 <script>
+const axios = require('axios');
+
 import { format, render, cancel, register } from 'timeago.js';
 export default {
   name: 'SuppliersMap',
@@ -27,6 +29,14 @@ export default {
       loading: false,
       error: null,
     }
+  },
+    created () {
+    axios
+      .get('https://api-suppliers.herokuapp.com/api/suppliers')
+      .then(response => {this.suppliers = response.data})
+      /*.catch(error => {console.log(error)
+      this.errored = true})
+      .finally(() => this.loading = false)*/
   }
 }
 </script>
